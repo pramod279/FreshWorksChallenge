6de7e5e82295d4827714496faa005707030cd440
Pramod Selvaraj <pramod.selvaraj@experionglobal.com>
2021-10-01 15:55:05 +0530

    Refactored Packages

--- app/src/main/java/com/freshworks/challenge/utilities/Constants.kt
+++ app/src/main/java/com/freshworks/challenge/utilities/Constants.kt
@@ -6,10 +6,12 @@ package com.freshworks.challenge.utilities
  *
  * Constants used throughout the app.
  */
-const val TRENDING_PAGE_INDEX = 0
-const val FAVOURITES_PAGE_INDEX = 1
-const val DATABASE_NAME = "favourites-db"
-const val DEFAULT_PAGE_INDEX = 1
-const val DEFAULT_PAGE_LIMIT = 10
-const val NETWORK_PAGE_SIZE = 20
-var PAGE_OFFSET = 0
+object Constants {
+    const val TRENDING_PAGE_INDEX = 0
+    const val FAVOURITES_PAGE_INDEX = 1
+    const val DATABASE_NAME = "favourites-db"
+    const val DEFAULT_PAGE_INDEX = 1
+    const val DEFAULT_PAGE_LIMIT = 10
+    const val NETWORK_PAGE_SIZE = 20
+    var PAGE_OFFSET = 0
+}
